2423032d7fec0b93576ce74ce7387a6019563198
Dana Woodman <dana@danawoodman.com>
2016-04-11 16:10:35 -0700

    Better events for serial issues.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
+import connect from './connect'
 import express from 'express'
 import http from 'http'
+import listDevices from './list'
 import serialport, { SerialPort } from 'serialport'
 import socketio from 'socket.io'
-import listDevices from './list'
-import connect from './connect'
 
 const app = express()
 const server = http.Server(app)
@@ -13,9 +13,68 @@ io.on('connection', (socket) => {
   console.log('a user connected')
 
   // List serial ports
-  listDevices(io)
+  serialport.list((err, ports) => {
+    console.log('available ports', ports)
+    io.emit('available ports', ports)
+  })
 
-  socket.on('connect to device', connect(socket))
+  socket.on('error', (err) => {
+    console.error('Socket error:', err.stack)
+  })
+
+  socket.on('connect to device', (device, onOpen) => {
+
+    const conn = new SerialPort(device, {
+      baudrate: 115200,
+      openImmediately: false,
+      parser: serialport.parsers.readline('\r\n')
+    })
+
+    conn.on('open', () => {
+
+      // Trigger callback when open
+      onOpen(null)
+
+      console.log('serial connection opened')
+      socket.emit('serial connect')
+
+      socket.on('send command', (data, callback) => {
+        console.log('recieved command from client:', data)
+
+        conn.write(data + '\r\n', (err, result) => {
+          if (err) {
+            console.log('err', err)
+            return callback(err)
+          }
+          console.log('result', result)
+          callback(null)
+        })
+      })
+    })
+
+    conn.on('data', (data) => {
+      console.log('received data from device:', data)
+      socket.emit('response', data)
+    })
+
+    conn.on('error', (err) => {
+      console.error('serial error', err)
+      socket.emit('serial error', error)
+    })
+
+    conn.on('close', async () => {
+      console.log('serial connection closed')
+      socket.emit('serial disconnect')
+      //await attemptReconnect(conn, socket)
+    })
+
+    socket.on('disconnect from device', (callback) => {
+      console.log('disconnect from device')
+      socket.emit('serial disconnect')
+      conn.close()
+      callback(null)
+    })
+  })
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
